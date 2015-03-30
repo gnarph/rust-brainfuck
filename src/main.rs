efce9e4c3305f5d7eb55a8af781011cacda681ca
Gord Larson <gordlarson@gmail.com>
2015-03-30 15:51:08 -0700

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+mod fuckvm;
+
 fn get_arg_one() -> String {
     // Can't just get the args as an array for some reason
     let mut args: Vec<String> = Vec::new();
@@ -14,5 +16,7 @@ fn get_arg_one() -> String {
 
 fn main() {
     let instructions = get_arg_one();
-    println!("{}", instructions);
+    println!("Got: {}", instructions);
+    let jump_map = fuckvm::create_jump_map(instructions.clone());
+    let vm = fuckvm::VM::new(instructions, jump_map);
 }
