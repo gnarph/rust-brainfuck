6d8b3fa4998eca0567a52206dd0592042e2b1529
Gord Larson <gordlarson@gmail.com>
2015-03-30 14:43:17 -0700

    move arguments stuff into a function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-fn main() {
+fn get_arg_one() -> String {
     // Can't just get the args as an array for some reason
     let mut args: Vec<String> = Vec::new();
     for argument in env::args_os() {
@@ -9,6 +9,10 @@ fn main() {
             Err(e) => println!("{:?}", e),
         }
     }
-    let instructions = args[1].clone();
+    args[1].clone()
+}
+
+fn main() {
+    let instructions = get_arg_one();
     println!("{}", instructions);
 }
